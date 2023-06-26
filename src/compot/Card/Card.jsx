dcb64f95d2e2be92644b71c858aedb872f44a2ab
Tymur <osypenkotymur@gmail.com>
2023-06-26 17:43:19 +0300

    Описание ваших изменений

--- src/compot/Card/Card.jsx
+++ src/compot/Card/Card.jsx
@@ -4,8 +4,17 @@ import style from './Card.module.css'
 const Card = () => {
   return (
     <div>
-        <div className={style.card1}><img src="../..img/img1.png" alt="" /></div>
-        <div className={style.card2}></div>
+        <div className={style.card1}>
+          <img src="src/compot/Card/img1.png" alt="" className={style.img} />
+          <div className={style.text}>0000 0000 0000 0000</div>
+          <div className={style.name}>Tymur Bogdanovich</div>
+          <div className={style.date}>00/00</div>
+        </div>
+        
+        <div className={style.card2}>
+        <img src="src/compot/Card/img2.png" alt="" className={style.img}/>
+        <div className={style.text2}>000</div>
+        </div>
     </div>
   )
 }
